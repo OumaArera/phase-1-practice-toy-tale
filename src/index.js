35d377f7006b5afa8cd4f70889cb86a2484770b0
OumaArera <johnouma999@gmail.com>
2024-01-01 12:01:54 +0300

    feat: fetch data from server, post new toy to the server

--- src/index.js
+++ src/index.js
@@ -13,3 +13,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch("http://localhost:3000/toys")
+.then(res => res.json())
+.then(data => data.forEach(element => displayAllToy(element)))
+.catch(error=> console.log(error))
+
+const displayAllToy = toyName => {
+  let card = document.createElement("li");
+  card.setAttribute("data-toy-id", toyName.id);
+  card.id = "toy";
+  card.innerHTML = `
+  <div class = "c-container">
+    <h2 class="name-t">${toyName.name}</h2>
+    <img src='${toyName.image}' class="toy-avatar" >
+      <p>Likes: <span> ${toyName.likes}</span></p>
+      <button class="like-btn" id="${toyName.id}">Like ❤️</button>
+  </div>
+  `;
+  document.querySelector("#toy-collection").appendChild(card)
+}
+
+let name = document.querySelector(".input-text");
+let image = document.querySelector("#inputText");
+
+let newToy = {
+  name: name,
+  image: image,
+  likes: 0
+}
+const addNewToy = () => {
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newToy),
+  })
+  .then(res => res.json())
+  .then(data => displayAllToy(data))
+  .catch(err => console.log(err))
+}
+document.querySelector(".submit").addEventListener('click', event => {event.preventDefault();addNewToy()})
