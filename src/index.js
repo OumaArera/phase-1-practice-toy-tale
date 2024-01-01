774f35d4711c768671faff32b13d8e1814564b5c
OumaArera <johnouma999@gmail.com>
2024-01-01 13:09:29 +0300

    fix: update likes

--- src/index.js
+++ src/index.js
@@ -18,33 +18,38 @@ fetch("http://localhost:3000/toys")
 .then(res => res.json())
 .then(data => data.forEach(element => displayAllToy(element)))
 .catch(error=> console.log(error))
-
+// Displays all the toy in the server
 const displayAllToy = toyName => {
+  // Create a list card
   let card = document.createElement("li");
   card.setAttribute("data-toy-id", toyName.id);
   card.id = "toy";
+  // Global variable that assigns a variable to toy id
   toyId = toyName.id;
+  // Global variable that assign currentLikes to the likes in the server at a particular id
   currentLikes = toyName.likes;
+  // Create an HTML card
   card.innerHTML = `
-  <div class = "c-container">
     <h2 class="name-t">${toyName.name}</h2>
     <img src='${toyName.image}' class="toy-avatar" >
       <p id='likes'>Likes: <span> ${toyName.likes}</span></p>
       <button class="like-btn" id="${toyName.id}">Like ❤️</button>
-  </div>
   `;
+  // Append card as a child
   document.querySelector("#toy-collection").appendChild(card)
 }
-
+// Creating new name and image
 let newName = document.querySelector(".input-text");
 let newImage = document.querySelector("#inputText");
-
+// New toy
 let newToy = {
   name: newName,
   image: newImage,
   likes: 0
 }
+// Function posts new toys to the server
 const addNewToy = () => {
+  // Fetches the data in the server and POST a new toy
   fetch("http://localhost:3000/toys", {
     method: "POST",
     headers: {
@@ -56,13 +61,15 @@ const addNewToy = () => {
   .then(data => displayAllToy(data))
   .catch(err => console.log(err))
 }
+// Listens to click then calls addNewToy() after preventing default page behavior
 document.querySelector(".submit").addEventListener('click', event => {event.preventDefault();addNewToy()})
-
+// Add event listener to execute updateLikes()
+document.querySelector(".like-btn").addEventListener('click', e =>{e.preventDefault();updateLikes()})
+// Function updates likes by updating the server.
 const updateLikes = () =>{
-  document.querySelector(".like-btn").addEventListener('click', e =>{
-    e.preventDefault()
+    // Current likes plus 1 like
     currentLikes += 1;
-
+    // Fetches the current data in the server at the given id
     fetch(`http://localhost:3000/toys/${toyId}`, {
       method: "PATCH",
       headers: {
@@ -74,5 +81,4 @@ const updateLikes = () =>{
     .then(res => res.json())
     .then(() => document.querySelector("#likes").textContent = `Likes: ${currentLikes}`)
     .catch(err => console.log(err))
-  })
 }
